42246f726dd4274b0748967ccdc2be0d7a552b33
USER_NAME <berkayster@gmail.com>
2024-10-22 18:38:41 -0500

    Edit : color changes

--- Base/base.gd
+++ Base/base.gd
@@ -8,8 +8,12 @@ var current_health: int:
 	set(health_in):
 		current_health = health_in
 		print("health was changed")
-		label.text = str(current_health)
+		label.text = str(current_health) + "/" + str(max_health)
 
+		var red: Color = Color.RED
+		var white: Color = Color.WHITE
+		label.modulate = red.lerp(white, float(current_health) / float(max_health))
+		
 		if current_health < 1:
 			get_tree().reload_current_scene()
 
@@ -18,6 +22,5 @@ func _ready() -> void:
 	pass
 
 func take_damage() -> void:
-	max_health -= 1
-	current_health = max_health
+	current_health -= 1
 	pass
