7e2f3f29359131bf45799dfca36696d9081bc07a
USER_NAME <berkayster@gmail.com>
2024-10-24 19:11:30 -0500

    Edit : click and cursors

--- RayPickerCamera/ray_picker_camera.gd
+++ RayPickerCamera/ray_picker_camera.gd
@@ -12,10 +12,14 @@ func _process(delta: float) -> void:
 	ray_cast_3d.force_raycast_update()
 
 	if ray_cast_3d.is_colliding():
+		Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
 		var collider = ray_cast_3d.get_collider()
 		if collider is GridMap:
-			var collision_point = ray_cast_3d.get_collision_point()
-			var cell = gridMap.local_to_map(collision_point)
-			if gridMap.get_cell_item(cell) == 0:
-				gridMap.set_cell_item(cell, 1)
-				print("Test")
+			if Input.is_action_just_pressed("click"):
+				var collision_point = ray_cast_3d.get_collision_point()
+				var cell = gridMap.local_to_map(collision_point)
+				if gridMap.get_cell_item(cell) == 0:
+					gridMap.set_cell_item(cell, 1)
+					print("Test")
+	else:
+		Input.set_default_cursor_shape(Input.CURSOR_ARROW)
