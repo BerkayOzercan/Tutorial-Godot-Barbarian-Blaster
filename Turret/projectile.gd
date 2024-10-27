8a294df22f6ae4e23b3bbde3311d34e75c9b0b35
USER_NAME <berkayster@gmail.com>
2024-10-27 18:53:55 -0500

    Edit : damaging enemies

--- Turret/projectile.gd
+++ Turret/projectile.gd
@@ -4,8 +4,17 @@ var direction := Vector3.FORWARD
 
 @export var speed: float = 30.0
 
+var damage: int = 25
+
 func _physics_process(delta: float) -> void:
 	position += direction * delta * speed
 
 func _on_timer_timeout() -> void:
 	queue_free()
+
+
+func _on_area_entered(area:Area3D) -> void:
+	if area.is_in_group("enemy_area"):
+		print(area)
+		area.get_parent().current_health -= damage
+		queue_free()
